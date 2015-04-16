eb8e923e20f03f02a882b4b7f0755c6daf3b2d62
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-04-16 20:00:01 +0000

    Creating branch for BioC 3.1 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_1/madman/Rpacks/BiocInstaller@102593 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/biocLite.R
@@ -0,0 +1,150 @@
+.mbniFilter <-
+    function(pkgs, ..., type=getOption("pkgType"))
+{
+    repos <- getOption("repos")
+    ## MBNI packages not always available
+    doing <- character()
+    if ((type %in% c("mac.binary", "mac.binary.leopard")) &&
+        ("MBNI" %in% names(repos)))
+    {
+        url <- contrib.url(repos[["MBNI"]])
+        doing <- intersect(pkgs, row.names(available.packages(url)))
+        if (length(doing)) {
+            pkgNames <- paste(sQuote(doing), collapse=", ")
+            .message("MBNI Brain Array packages %s are not available as
+                      Mac binaries, use biocLite with type='source'",
+                     pkgNames)
+        }
+    }
+    setdiff(pkgs, doing)
+}
+
+.reposInstall <-
+    function(pkgs, lib, ...)
+{
+    ## non-'github' packages
+    doing <- grep("/", pkgs, invert=TRUE, value=TRUE)
+    if (length(doing)) {
+        pkgNames <- paste(sQuote(doing), collapse=", ")
+        .message("Installing package(s) %s", pkgNames)
+        install.packages(pkgs=doing, lib=lib, ...)
+    }
+    setdiff(pkgs, doing)
+}
+
+.githubInstall <-
+    function(pkgs, ...)
+{
+    doing <- grep("/", pkgs, value=TRUE)
+    if (length(doing)) {
+        pkgNames <- paste(sQuote(doing), collapse=", ")
+        if (!requireNamespace("devtools", quietly=TRUE))
+            .stop("github installation of %s only available after
+                   biocLite(\"devtools\")", pkgNames)
+        .message("Installing github package(s) %s", pkgNames)
+        devtools::install_github(doing, ...)
+    }
+    setdiff(pkgs, doing)
+}
+
+.biocLiteInstall <-
+    function(pkgs, repos, ask, suppressUpdates, siteRepos=character(),
+             lib.loc=.libPaths(), lib=.libPaths()[1], ...)
+{
+    if (!missing(repos))
+        .stop("'repos' argument to 'biocLite' not allowed")
+
+    if (!(is.character(suppressUpdates) || is.logical(suppressUpdates)) ||
+        (is.logical(suppressUpdates) && 1L != length(suppressUpdates)))
+        .stop("'suppressUpdates' must be character() or logical(1)")
+
+    biocMirror <- getOption("BioC_mirror", "http://bioconductor.org")
+    .message("BioC_mirror: %s", biocMirror)
+
+    version <- getRversion()
+    thisRVer <- sprintf("%d.%d", version$major, version$minor)
+    .message("Using Bioconductor version %s (BiocInstaller %s), R version %s.",
+             biocVersion(), packageVersion("BiocInstaller"), version)
+
+    if (!suppressPackageStartupMessages(require("utils", quietly=TRUE)))
+        .stop("failed to load package 'utils'")
+
+    orepos <- options(repos=biocinstallRepos(siteRepos))
+    on.exit(options(orepos))
+
+    if (length(pkgs)) {
+        todo <- .mbniFilter(pkgs, ...)
+        todo <- .reposInstall(todo, lib=lib, ...)
+        todo <- .githubInstall(todo, ...)
+    }
+
+    ## early exit if suppressUpdates
+    if (is.logical(suppressUpdates) && suppressUpdates)
+        return(invisible(pkgs))
+    pkgsToUpdate <- old.packages(lib.loc=lib.loc)
+    if (is.null(pkgsToUpdate))
+        return(invisible(pkgs))
+
+    if (!is.logical(suppressUpdates)) {
+        pkgsToUpdate <-
+            filterPackagesToUpdate(suppressUpdates, pkgsToUpdate)
+        suppressUpdates <- FALSE
+    }
+
+    oldPkgs <- pkgsToUpdate
+    if (nrow(oldPkgs)) {
+        pkgList <- paste(oldPkgs[,"Package"], collapse="', '")
+        if (ask==TRUE) {
+            .message("Old packages: '%s'", pkgList)
+
+            answer <-
+                .getAnswer("Update all/some/none? [a/s/n]: ",
+                           allowed = c("a", "A", "s", "S", "n", "N"))
+
+            switch(answer,
+                   a = update.packages(oldPkgs=oldPkgs, ask=FALSE),
+                   s = update.packages(oldPkgs=oldPkgs, ask=TRUE),
+                   n = invisible(pkgs))   
+        } else {
+            .message("Updating packages '%s'", pkgList)
+            update.packages(oldPkgs=oldPkgs, ask=ask)
+        }
+    }
+
+    invisible(pkgs)
+}
+
+.getAnswer <- function(msg, allowed)
+{
+    if (interactive()) {
+        repeat {
+            cat(msg)
+            answer <- readLines(n = 1)
+            if (answer %in% allowed)
+                break
+        }
+        tolower(answer)
+    } else {
+        "n"
+    }
+}
+
+biocLite <-
+    function(pkgs=c("Biobase","IRanges","AnnotationDbi"),
+             suppressUpdates=FALSE,
+             suppressAutoUpdate=FALSE,
+             siteRepos=character(), ask=TRUE, ...)
+{
+    if (missing(pkgs))   # biocLite() update w/out installing defaults
+        pkgs <- pkgs[!pkgs %in% rownames(installed.packages())]
+    if (!suppressAutoUpdate && !.isCurrentBiocInstaller()) {
+        on.exit(.updateBiocInstaller(pkgs, ask=ask,
+                                     suppressUpdates=suppressUpdates,
+                                     siteRepos=siteRepos, ...))
+    } else if ("BiocUpgrade" %in% pkgs) {
+        .biocUpgrade()
+    } else {
+        .biocLiteInstall(pkgs, ask=ask, siteRepos=siteRepos,
+                         suppressUpdates=suppressUpdates, ...)
+    }
+}
---
+++ R/zzz.R
@@ -0,0 +1,64 @@
+## The following values are updated with each Bioc release; see .onLoad
+BIOC_VERSION <- package_version("3.1")     # Bioc version for this package
+R_VERSION_MAX <- package_version("3.3.0")  # Maximum version of R for
+                                           # this version of BiocInstaller
+IS_USER <- FALSE                           # TRUE if this version of
+                                           # Bioconductor is the
+                                           # current release version
+IS_END_OF_LIFE <- FALSE                    # TRUE if this version of
+                                           # Bioconductor is no longer
+                                           # the release version
+
+IS_UPGRADEABLE <- FALSE                    # TRUE if a more recent
+                                           # version (release or
+                                           # devel) of Bioconductor is
+                                           # available for this
+                                           # version of R
+UPGRADE_IS_DEVEL <- TRUE                   # TRUE if UPGRADE_VERSION
+                                           # is for devel use only
+IS_DOWNGRADEABLE <- FALSE                  # TRUE if an older version
+                                           # (release or devel) of
+                                           # Bioconductor is available
+                                           # for this version of R
+UPGRADE_VERSION <- package_version("3.2")  # Bioconductor version for
+                                           # upgrade, if
+                                           # IS_UPGRADEABLE == TRUE
+DOWNGRADE_VERSION <- package_version("3.0") # Bioconductor version for
+                                           # downgrade, if
+                                           # IS_DOWNGRADEABLE == # TRUE
+
+NEXT_R_DEVEL_VERSION <- "3.3.0" # next (not-yet-supported) version of R
+
+## Change when the status of MBNI changes. 
+## Make sure this change is propagated to users, even 
+## if builds have stopped for a particular version of BioC.
+## See biocLite.R:biocinstallRepos to include / exclude package types
+includeMBNI <- FALSE
+mbniUrl <- "http://brainarray.mbni.med.umich.edu/bioc"
+
+globalVariables("repos")           # used in 'bootstrap' functions
+
+.onAttach <-
+    function(libname, pkgname) 
+{
+    Rversion <- getRversion()
+    .message("Bioconductor version %s (BiocInstaller %s), ?biocLite for help",
+             biocVersion(), packageVersion("BiocInstaller"))
+    if (IS_END_OF_LIFE) {
+        if (IS_UPGRADEABLE)
+            .message("A newer version of Bioconductor is available for
+                      this version of R, ?BiocUpgrade for help")
+        else
+            .message("A new version of Bioconductor is available after
+                      installing the most recent version of R; see
+                      http://bioconductor.org/install")
+    } else if (Rversion >= R_VERSION_MAX) {
+        if (Rversion >= NEXT_R_DEVEL_VERSION)
+            .message("Bioconductor does not yet support R version %s", Rversion)
+        else
+            .warning("BiocInstaller version %s is too old for R version %s;
+                      remove.packages(\"BiocInstaller\") then
+                      source(\"http://bioconductor.org/biocLite.R\")",
+                     BIOC_VERSION, Rversion)
+    }
+}
